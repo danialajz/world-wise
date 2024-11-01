67100f204db1fcfaf4e2d74864ec87a6006cbbf6
danial alijanianzadeh <88320221+danialajz82@users.noreply.github.com>
2024-11-01 14:24:38 +0330

    add json-server

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -7,8 +7,22 @@ import AppLayout from "./pages/AppLayout";
 import Homepage from "./pages/HomePage";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
+const BASE_URL = "http://localhost:9000";
 
 function App() {
+  const [cities, setCities] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
