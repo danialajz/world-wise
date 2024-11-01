e840ed25bcbf79a2246f846508f7d23c40d7dd61
danial alijanianzadeh <88320221+danialajz82@users.noreply.github.com>
2024-11-01 11:32:06 +0330

    add Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
