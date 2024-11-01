e2f8d54787342ee2189d77e8f53d4da016141e99
danial alijanianzadeh <88320221+danialajz82@users.noreply.github.com>
2024-11-01 14:48:42 +0330

    add message and copy json file

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import styles from "./CityList.module.css";
-function CityList() {
-  return <ul className={styles.cityList}>CityList</ul>;
+import Spinner from "./Spinner";
+import CityItem from "./CityItem";
+import Message from "./Message";
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+  if (!cities.lenght) return <Message />;
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
 }
 
 export default CityList;
